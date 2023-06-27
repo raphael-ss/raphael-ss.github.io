bcb18b534971fd010b74d19e839c3e7673da402a
Raphael Suarez <raphaelsoaresbrasil@hotmail.com>
2023-06-27 18:52:12 -0300

    updated python basic

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -70,7 +70,7 @@ const config = {
         title: 'DataComb',
         logo: {
           alt: 'datacomb-logo',
-          src: 'img/logo.svg',
+          src: 'img/logo.png',
         },
         items: [
           {
